23803cab62bf2e7cc2634b2ffb35d37738e12e77
negbook <negbook@gmail.com>
2021-06-20 01:01:44 +0800

    Add files via upload

---
+++ example.lua
@@ -0,0 +1,67 @@
+FlowDetector = {}
+function FlowCheck(name,inputValue)
+	if not FlowDetector[name] then 
+		FlowCheckCreate(name)
+	end 
+	local FD = FlowDetector[name]
+	local new = FD.temp[2]
+	local old = FD.temp[1]
+	local refreshFD = function(n) FD.temp[1] = n end
+	if not FD then 
+		return 
+	end 
+	new = inputValue
+	if old == nil then 
+		FlowOnInitialise(name,new) --無默認值並由nil首次載入
+		refreshFD(new) --由nil賦予新值
+	elseif old == new then --一樣
+		FlowOnSame(name)
+	elseif old ~= new then 
+		if new == nil then 
+			error("WHY IS HERE GOT NIL?",2)
+		else 
+			FlowOnChange(name,old,new) --有變更
+			refreshFD(new) --舊變新
+		end 
+	end 
+end 
+
+function FlowCheckCreate(name,defaultValue)
+	if not FlowDetector[name] then FlowDetector[name] = {} end 
+	FlowDetector[name].temp = {defaultValue,defaultValue} 
+end 
+
+function FlowCheckDelete(name)
+	FlowDetector[name] = nil
+	collectgarbage()
+end
+
+function FlowOnInitialise(name,thefirstValue)
+	
+end 
+
+function FlowOnSame(name)
+	
+end
+
+function FlowOnChange(name,fromValue,toValue)
+	local case = {} --cfx-switchcase by negbook https://github.com/negbook/cfx-switchcase/blob/main/cfx-switchcase.lua
+    local switch = setmetatable({},{__call=function(a,b)case=setmetatable({},{__call=function(a,b)return a[b]end,__index=function(a,c)return c and c==b and setmetatable({},{__call=function(a,d)d()end})or function()end end})return a[b]end,__index=function(a,c)return setmetatable({},{__call=function(a,...)end})end})
+    switch(name)(
+        case('coords')(function()
+            print('Position Updated:from ('..type(fromValue)..') '..tostring(fromValue)..'  to ('..type(toValue)..') '..tostring(toValue))
+        end),
+        case('health')(function()
+            print('Health Updated:from ('..type(fromValue)..') '..tostring(fromValue)..'  to ('..type(toValue)..') '..tostring(toValue))
+        end)
+    )
+end 
+
+FlowCheckCreate('health',"hello")
+CreateThread(function()
+    
+    while true do Wait(1000)
+        FlowCheck('coords',GetEntityCoords(PlayerPedId()))
+        FlowCheck('health',GetEntityHealth(PlayerPedId()))
+    end 
+end)
