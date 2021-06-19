f93a9b5a052d90b6655ba2e46fced69aa027c00a
negbook <negbook@gmail.com>
2021-06-20 00:45:27 +0800

    Update flowdetector.lua

--- flowdetector.lua
+++ flowdetector.lua
@@ -1,29 +1,29 @@
 FlowDetector = {}
 function FlowCheck(name,x)
-    if not FlowDetector[name] then 
-        FlowCheckCreate(name)
-    end 
-    local FD = FlowDetector[name]
-    local new = FD.temp[2]
-    local old = FD.temp[1]
+	if not FlowDetector[name] then 
+		FlowCheckCreate(name)
+	end 
+	local FD = FlowDetector[name]
+	local new = FD.temp[2]
+	local old = FD.temp[1]
 	local refreshFD = function(n) FD.temp[1] = n end
 	if not FD then 
 		return 
 	end 
 	new = x
-    if old == nil then 
-        FlowOnInitialise(name,new) --無默認值並由nil首次載入
-        refreshFD(new) --由nil賦予新值
-    elseif old == new then --一樣
+	if old == nil then 
+		FlowOnInitialise(name,new) --無默認值並由nil首次載入
+		refreshFD(new) --由nil賦予新值
+	elseif old == new then --一樣
 		FlowOnSame(name)
-    elseif old ~= new then 
-        if new == nil then 
-            error("WHY IS HERE GOT NIL?",2)
-        else 
-            FlowOnChange(name,old,new) --有變更
-            refreshFD(new) --舊變新
-        end 
-    end 
+	elseif old ~= new then 
+		if new == nil then 
+			error("WHY IS HERE GOT NIL?",2)
+		else 
+			FlowOnChange(name,old,new) --有變更
+			refreshFD(new) --舊變新
+		end 
+	end 
 end 
 
 function FlowCheckCreate(name,defaultValue)
@@ -37,7 +37,7 @@ function FlowCheckDelete(name)
 end
 
 function FlowOnInitialise(name,thefirstValue)
-    --print(name..' The first value: '..thefirstValue)
+	--print(name..' The first value: '..thefirstValue)
 end 
 
 function FlowOnSame(name)
