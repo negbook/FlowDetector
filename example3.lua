a348f4ad511c7607106f919a5924ceeabcfbac9f
negbook <negbook@gmail.com>
2021-06-21 18:03:02 +0800

    Delete example3.lua

--- example3.lua
+++
@@ -1,9 +0,0 @@
-CreateThread(function()
-    FlowCheckCreate("Pause",false,function(name,old,new)
-        print(name.." change")
-    end )
-    while true do
-        IsPause = FlowCheck("Pause",IsPauseMenuActive())
-        Citizen.Wait(332)
-    end
-end)
