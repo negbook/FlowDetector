7cf77fa54cda25d3c7fcaf09417f9fca1df54b52
negbook <negbook@gmail.com>
2021-08-10 21:27:52 +0800

    Update example.lua

--- example.lua
+++ example.lua
@@ -1,28 +1,8 @@
-
 CreateThread(function()
-    FlowDetector.Link({"A","B"},"Pause")
-    FlowDetector.Create('Pause')
-    FlowDetector.Create('A')
-    FlowDetector.Create('B')
-    FlowDetector.Create('C')
-    FlowDetector.Link("C","Pause")
-    FlowDetector.Register("Pause",'change',function(name,old,new,islinked,linkto)
-        if islinked then 
-        print(linkto.." change ",tostring(old),tostring(new) .. 'from '..name)
-        end 
-    end )
-    FlowDetector.Register("A",'change',function(name,old,new,islinked,linkto)
-        if not islinked then 
-        print(name.." change ",tostring(old),tostring(new) )
-        end 
-    end )
-        FlowDetector.Check("Pause",1)
-        IsPause = FlowDetector.Check("A",IsPauseMenuActive())
-        FlowDetector.Check("Pause",2)
-    while true do
-        IsPause = FlowDetector.Check("A",IsPauseMenuActive())
-        FlowDetector.Check("B",not IsPauseMenuActive())
-        FlowDetector.Check("C",999+math.random())
-        Citizen.Wait(332)
-    end
+	while true do Wait(1000)
+		Flow.Check(IsPauseMenuActive).OnChangeWhatever(function(data1,data2)
+			print("OnChangeWhatever",table.unpack(data1),table.unpack(data2))
+		end)
+
+	end 
 end)
