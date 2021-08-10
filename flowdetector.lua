cf1ecf6a1a8bcb3e8b5ae0f278344116e337029c
negbook <negbook@gmail.com>
2021-08-11 03:17:35 +0800

    Update flowdetector.lua

--- flowdetector.lua
+++ flowdetector.lua
@@ -2,63 +2,64 @@ local Flow = {}
 
 Flow._temp_ = {old={},new={},oldDataSource={},newDataSource={}}
 
-Flow.CheckNative = function(fn,...)
+Flow.CheckNative = function(name,fn,...)
 	local args = {...} 
 	local lastarg = args[#args]
 	local cb = type(lastarg) == "function" and lastarg or nil
 	table.remove(args,#args)
 	local _fn = fn 
 	local tt = {_fn(table.unpack(args))}
-	Flow._temp_.new[fn] = json.encode(tt)
-	Flow._temp_.newDataSource[fn] = tt
-	if Flow._temp_.old[fn] ~= Flow._temp_.new[fn] then 
-		local t = Flow._temp_.oldDataSource[fn] and Flow._temp_.oldDataSource[fn] or nil 
-		local t2 = Flow._temp_.newDataSource[fn]
+	Flow._temp_.new[name] = json.encode(tt)
+	Flow._temp_.newDataSource[name] = tt
+	if Flow._temp_.old[name] ~= Flow._temp_.new[name] then 
+		local t = Flow._temp_.oldDataSource[name] and Flow._temp_.oldDataSource[name] or nil 
+		local t2 = Flow._temp_.newDataSource[name]
 		if t == nil  then 
-			Flow._temp_.old[fn] = Flow._temp_.new[fn]
-			Flow._temp_.oldDataSource[fn] = Flow._temp_.newDataSource[fn]
+			Flow._temp_.old[name] = Flow._temp_.new[name]
+			Flow._temp_.oldDataSource[name] = Flow._temp_.newDataSource[name]
 			
 			if cb then cb("OnInit",t2) end 
 			if cb then cb("OnChange","null",t2) end 
 		else 
-			Flow._temp_.old[fn] = Flow._temp_.new[fn]
-			Flow._temp_.oldDataSource[fn] = Flow._temp_.newDataSource[fn]
+			Flow._temp_.old[name] = Flow._temp_.new[name]
+			Flow._temp_.oldDataSource[name] = Flow._temp_.newDataSource[name]
 
 			if cb then cb("OnChange",table.unpack(t),table.unpack(t2)) end 
 		end 
 	else 
-		local t = Flow._temp_.newDataSource[fn]
+		local t = Flow._temp_.newDataSource[name]
 		if cb then cb("OnSame",table.unpack(t)) end 
 	end 
 end 
 
-Flow.CheckNativeChange = function(fn,...)
+Flow.CheckNativeChange = function(name,fn,...)
+	
 	local args = {...} 
 	local lastarg = args[#args]
 	local cb = type(lastarg) == "function" and lastarg or nil
 	table.remove(args,#args)
 	local _fn = fn 
 	local tt = type(_fn) == "function" and {_fn(table.unpack(args))} or _fn
-	Flow._temp_.new[fn] = json.encode(tt)
-	Flow._temp_.newDataSource[fn] = tt
-	if Flow._temp_.old[fn] ~= Flow._temp_.new[fn] then 
-		local t = Flow._temp_.oldDataSource[fn] and Flow._temp_.oldDataSource[fn] or nil 
-		local t2 = Flow._temp_.newDataSource[fn]
+	Flow._temp_.new[name] = json.encode(tt)
+	Flow._temp_.newDataSource[name] = tt
+	if Flow._temp_.old[name] ~= Flow._temp_.new[name] then 
+		local t = Flow._temp_.oldDataSource[name] and Flow._temp_.oldDataSource[name] or nil 
+		local t2 = Flow._temp_.newDataSource[name]
 		if t == nil  then 
 			--print("init",t,"to",table.unpack(t2))
-			Flow._temp_.old[fn] = Flow._temp_.new[fn]
-			Flow._temp_.oldDataSource[fn] = Flow._temp_.newDataSource[fn]
+			Flow._temp_.old[name] = Flow._temp_.new[name]
+			Flow._temp_.oldDataSource[name] = Flow._temp_.newDataSource[name]
 			if cb then cb("null",t2) end 
 		else 
 			--print("change",table.unpack(t),"to",table.unpack(t2))
-			Flow._temp_.old[fn] = Flow._temp_.new[fn]
-			Flow._temp_.oldDataSource[fn] = Flow._temp_.newDataSource[fn]
+			Flow._temp_.old[name] = Flow._temp_.new[name]
+			Flow._temp_.oldDataSource[name] = Flow._temp_.newDataSource[name]
 			if cb then cb(table.unpack(t),table.unpack(t2)) end 
 		end 
 	end 
 end 
 
-Flow.CheckNativeChangeVector = function(fn,...)
+Flow.CheckNativeChangeVector = function(name,fn,...)
 	local args = {...} 
 	local lastarg = args[#args]
 	local range = type(lastarg) == "function" and args[#args-1] or nil
@@ -66,22 +67,23 @@ Flow.CheckNativeChangeVector = function(fn,...)
 	table.remove(args,#args)
 	table.remove(args,#args)
 	local _fn = fn 
+	
 	local tt = _fn(table.unpack(args))
-	Flow._temp_.new[fn] = json.encode(tt)
-	Flow._temp_.newDataSource[fn] = tt
-	if Flow._temp_.old[fn] ~= Flow._temp_.new[fn] then
-		local t = Flow._temp_.oldDataSource[fn] and Flow._temp_.oldDataSource[fn] or nil 
-		local t2 = Flow._temp_.newDataSource[fn]
+	Flow._temp_.new[name] = json.encode(tt)
+	Flow._temp_.newDataSource[name] = tt
+	if Flow._temp_.old[name] ~= Flow._temp_.new[name] then
+		local t = Flow._temp_.oldDataSource[name] and Flow._temp_.oldDataSource[name] or nil 
+		local t2 = Flow._temp_.newDataSource[name]
 		if t == nil  then 
 			--print("init",t,"to",table.unpack(t2))
-			Flow._temp_.old[fn] = Flow._temp_.new[fn]
-			Flow._temp_.oldDataSource[fn] = Flow._temp_.newDataSource[fn]
+			Flow._temp_.old[name] = Flow._temp_.new[name]
+			Flow._temp_.oldDataSource[name] = Flow._temp_.newDataSource[name]
 			if cb then cb("null",t2) end 
 		else 
 			--print("change",table.unpack(t),"to",table.unpack(t2))
-			if #(Flow._temp_.newDataSource[fn] - Flow._temp_.oldDataSource[fn]) > range then 
-				Flow._temp_.old[fn] = Flow._temp_.new[fn]
-				Flow._temp_.oldDataSource[fn] = Flow._temp_.newDataSource[fn]
+			if #(Flow._temp_.newDataSource[name] - Flow._temp_.oldDataSource[name]) > range then 
+				Flow._temp_.old[name] = Flow._temp_.new[name]
+				Flow._temp_.oldDataSource[name] = Flow._temp_.newDataSource[name]
 				if cb then cb(t,t2) end 
 			end 
 		end 
@@ -89,17 +91,17 @@ Flow.CheckNativeChangeVector = function(fn,...)
 	return 
 end 
 
-Flow.CheckNativeSame = function(fn,...)
+Flow.CheckNativeSame = function(name,fn,...)
 	local args = {...} 
 	local lastarg = args[#args]
 	local cb = type(lastarg) == "function" and lastarg or nil
 	table.remove(args,#args)
 	local _fn = fn 
 	local tt = {_fn(table.unpack(args))}
-	Flow._temp_.new[fn] = json.encode(tt)
-	Flow._temp_.newDataSource[fn] = tt
-	if Flow._temp_.old[fn] == Flow._temp_.new[fn] then 
-		local t = Flow._temp_.newDataSource[fn]
+	Flow._temp_.new[name] = json.encode(tt)
+	Flow._temp_.newDataSource[name] = tt
+	if Flow._temp_.old[name] == Flow._temp_.new[name] then 
+		local t = Flow._temp_.newDataSource[name]
 		if cb then cb(table.unpack(t)) end 
 	end 
 end 
