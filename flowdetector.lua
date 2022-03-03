8200e8afa2361157f1f90ef215683b4cf4763f29
negbook <negbook@gmail.com>
2022-03-04 02:33:23 +0800

    rewrite

--- flowdetector.lua
+++ flowdetector.lua
@@ -1,187 +1,66 @@
 local Flow = {}
-
-Flow._temp_ = {old={},new={},oldDataSource={},newDataSource={}}
+local Flows = {}
+local e = {}
+local function newObject(value)
+	local haschange = false 
+	return function(action,v,on) 
+		if action == 'get' then 
+			return value 
+		elseif action == 'set' then 
+			if value ~= v then 
+				if (on or e).change then 
+					on.change(value,v) 
+				end 
+				haschange = true
+				value = v 
+				
+			else 
+				if (on or e).same then 
+					on.same(value,v) 
+				end 
+				haschange = false 
+			end 
+		elseif action == 'has' then 
+			return value ~= nil
+		elseif action == 'haschange' then 
+			local r = haschange
+			haschange = false 
+			return r
+		else 
+			error "invalid action"
+		end 
+	end 
+end 
 
 Flow.CheckNative = function(name,fn,...)
-	local args = {...} 
-	local lastarg = args[#args]
-	local cb = type(lastarg) == "function" and lastarg or nil
-	table.remove(args,#args)
-	local _fn = fn 
-	local tt = {_fn(table.unpack(args))}
-	Flow._temp_.new[name] = json.encode(tt)
-	Flow._temp_.newDataSource[name] = tt
-	if Flow._temp_.old[name] ~= Flow._temp_.new[name] then 
-		local t = Flow._temp_.oldDataSource[name] and Flow._temp_.oldDataSource[name] or nil 
-		local t2 = Flow._temp_.newDataSource[name]
-		if t == nil  then 
-			Flow._temp_.old[name] = Flow._temp_.new[name]
-			Flow._temp_.oldDataSource[name] = Flow._temp_.newDataSource[name]
-			
-			if cb then cb("OnInit",t2) end 
-			if cb then cb("OnChange","null",t2) end 
-		else 
-			Flow._temp_.old[name] = Flow._temp_.new[name]
-			Flow._temp_.oldDataSource[name] = Flow._temp_.newDataSource[name]
-
-			if cb then cb("OnChange",table.unpack(t),table.unpack(t2)) end 
-		end 
-	else 
-		local t = Flow._temp_.newDataSource[name]
-		if cb then cb("OnSame",table.unpack(t)) end 
-	end 
-end 
-
-Flow.CheckNativeChange = function(name,fn,...)
-	
-	local args = {...} 
-	local lastarg = args[#args]
-	local cb = type(lastarg) == "function" and lastarg or nil
-	table.remove(args,#args)
-	local _fn = fn 
-	local tt = type(_fn) == "function" and {_fn(table.unpack(args))} or _fn
-	Flow._temp_.new[name] = json.encode(tt)
-	Flow._temp_.newDataSource[name] = tt
-	if Flow._temp_.old[name] ~= Flow._temp_.new[name] then 
-		local t = Flow._temp_.oldDataSource[name] and Flow._temp_.oldDataSource[name] or nil 
-		local t2 = Flow._temp_.newDataSource[name]
-		if t == nil  then 
-			--print("init",t,"to",table.unpack(t2))
-			Flow._temp_.old[name] = Flow._temp_.new[name]
-			Flow._temp_.oldDataSource[name] = Flow._temp_.newDataSource[name]
-			if cb then cb("null",t2) end 
-		else 
-			--print("change",table.unpack(t),"to",table.unpack(t2))
-			Flow._temp_.old[name] = Flow._temp_.new[name]
-			Flow._temp_.oldDataSource[name] = Flow._temp_.newDataSource[name]
-			if cb then cb(table.unpack(t),table.unpack(t2)) end 
+	local cb
+	local on = {}
+	local opts = {...}
+	local lastopts = opts[#opts] 
+	if type(lastopts) == 'function' then 
+		
+		cb = lastopts
+		table.remove(opts,#opts)
+		if not Flows[name] then 
+			Flows[name] = newObject(fn(table.unpack(opts))) 
+		end
+		cb(on)
+		if on.change or on.same then 
+			Flows[name]('set',fn(table.unpack(opts)),on)
 		end 
 	end 
+	
 end 
 
-Flow.CheckNativeChangeVector = function(name,fn,...)
-	local args = {...} 
-	local lastarg = args[#args]
-	local range = type(lastarg) == "function" and args[#args-1] or nil
-	local cb = type(lastarg) == "function" and lastarg or nil
-	table.remove(args,#args)
-	table.remove(args,#args)
-	local _fn = fn 
-	
-	local tt = _fn(table.unpack(args))
-	Flow._temp_.new[name] = json.encode(tt)
-	Flow._temp_.newDataSource[name] = tt
-	if Flow._temp_.old[name] ~= Flow._temp_.new[name] then
-		local t = Flow._temp_.oldDataSource[name] and Flow._temp_.oldDataSource[name] or nil 
-		local t2 = Flow._temp_.newDataSource[name]
-		if t == nil  then 
-			--print("init",t,"to",table.unpack(t2))
-			Flow._temp_.old[name] = Flow._temp_.new[name]
-			Flow._temp_.oldDataSource[name] = Flow._temp_.newDataSource[name]
-			if cb then cb("null",t2) end 
-		else 
-			--print("change",table.unpack(t),"to",table.unpack(t2))
-			if #(Flow._temp_.newDataSource[name] - Flow._temp_.oldDataSource[name]) > range then 
-				Flow._temp_.old[name] = Flow._temp_.new[name]
-				Flow._temp_.oldDataSource[name] = Flow._temp_.newDataSource[name]
-				if cb then cb(t,t2) end 
+CreateThread(function()
+	while true do Wait(1000)
+		Flow.CheckNative('haha',GetEntityCoords,PlayerPedId(),function(on)
+			on.change = function(oldvalue,newvalue)
+				print('change',oldvalue,newvalue)
+			end
+			on.same = function(oldvalue,newvalue)
+				print('same',oldvalue,newvalue)
 			end 
-		end 
+		end)
 	end 
-	return 
-end 
-
-Flow.CheckNativeSame = function(name,fn,...)
-	local args = {...} 
-	local lastarg = args[#args]
-	local cb = type(lastarg) == "function" and lastarg or nil
-	table.remove(args,#args)
-	local _fn = fn 
-	local tt = {_fn(table.unpack(args))}
-	Flow._temp_.new[name] = json.encode(tt)
-	Flow._temp_.newDataSource[name] = tt
-	if Flow._temp_.old[name] == Flow._temp_.new[name] then 
-		local t = Flow._temp_.newDataSource[name]
-		if cb then cb(table.unpack(t)) end 
-	end 
-end 
-
-Flow.Check = function(name,obj,...)
-	local args = {...} 
-	local lastarg = args[#args]
-	local cb = type(lastarg) == "function" and lastarg or nil
-	table.remove(args,#args)
-	local name = name
-	local tt = type(obj) == 'table' and {obj} or obj
-	Flow._temp_.new[name] = type(tt) == 'table' and json.encode(tt) or tt
-	Flow._temp_.newDataSource[name] = tt
-	if Flow._temp_.old[name] ~= Flow._temp_.new[name] then 
-		local t = Flow._temp_.oldDataSource[name] and Flow._temp_.oldDataSource[name] or nil 
-		local t2 = Flow._temp_.newDataSource[name]
-		if t == nil  then 
-			Flow._temp_.old[name] = Flow._temp_.new[name]
-			Flow._temp_.oldDataSource[name] = Flow._temp_.newDataSource[name]
-			
-			if cb then cb("OnInit",type(t2) == 'table' and table.unpack(t2) or t2) end 
-			if cb then cb("OnChange","null",type(t2) == 'table' and table.unpack(t2) or t2) end 
-		else 
-			Flow._temp_.old[name] = Flow._temp_.new[name]
-			Flow._temp_.oldDataSource[name] = Flow._temp_.newDataSource[name]
-
-			if cb then cb("OnChange",type(t) == 'table' and table.unpack(t) or t,type(t2) == 'table' and table.unpack(t2) or t2) end 
-		end 
-	else 
-		local t = Flow._temp_.newDataSource[name]
-		if cb then cb("OnSame",type(t) == 'table' and table.unpack(t) or t) end 
-	end 
-end 
-
-Flow.CheckChange = function(name,obj,...)
-	local args = {...} 
-	local lastarg = args[#args]
-	local cb = type(lastarg) == "function" and lastarg or nil
-	table.remove(args,#args)
-	local name = name
-	local tt = type(obj) == 'table' and {obj} or obj
-	Flow._temp_.new[name] = type(tt) == 'table' and json.encode(tt) or tt
-	Flow._temp_.newDataSource[name] = tt
-	if Flow._temp_.old[name] ~= Flow._temp_.new[name] then 
-		local t = Flow._temp_.oldDataSource[name] and Flow._temp_.oldDataSource[name] or nil 
-		local t2 = Flow._temp_.newDataSource[name]
-		if t == nil  then 
-			Flow._temp_.old[name] = Flow._temp_.new[name]
-			Flow._temp_.oldDataSource[name] = Flow._temp_.newDataSource[name]
-
-			if cb then cb("null",type(t2) == 'table' and table.unpack(t2) or t2) end 
-		else 
-			Flow._temp_.old[name] = Flow._temp_.new[name]
-			Flow._temp_.oldDataSource[name] = Flow._temp_.newDataSource[name]
-
-			if cb then cb(type(t) == 'table' and table.unpack(t) or t,type(t2) == 'table' and table.unpack(t2) or t2) end 
-		end 
-	end 
-end 
-
-Flow.CheckSame = function(name,obj,...)
-	local args = {...} 
-	local lastarg = args[#args]
-	local cb = type(lastarg) == "function" and lastarg or nil
-	table.remove(args,#args)
-	local name = name
-	local tt = type(obj) == 'table' and {obj} or obj
-	Flow._temp_.new[name] = type(tt) == 'table' and json.encode(tt) or tt
-	Flow._temp_.newDataSource[name] = tt
-	if Flow._temp_.old[name] == Flow._temp_.new[name] then 
-		local t = Flow._temp_.newDataSource[name]
-		if cb then cb(type(t) == 'table' and table.unpack(t) or t) end 
-	end 
-end 
-
-Flow.DeleteCheckNative = function(fn,cb)
-	if cb then cb(Flow._temp_.old[fn],Flow._temp_.new[fn]) end 
-	Flow._temp_.new[fn] = nil 
-    Flow._temp_.old[fn] = nil 
-end
-
-
-
+end)
