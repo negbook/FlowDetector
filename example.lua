cd4e539aa1f6a611b1f1b763e301227a00573e8b
negbook <negbook@gmail.com>
2022-03-04 02:35:02 +0800

    update

--- example.lua
+++ example.lua
@@ -1,18 +1,13 @@
+
 CreateThread(function()
 	while true do Wait(1000)
-		Flow.Check(IsPauseMenuActive).OnChange(function(datas1,datas2) 
-			print("OnChange",table.unpack(datas1),table.unpack(datas2))
+		Flow.CheckNative('haha',GetEntityCoords,PlayerPedId(),function(on)
+			on.change = function(oldvalue,newvalue)
+				print('change',oldvalue,newvalue)
+			end
+			on.same = function(oldvalue,newvalue)
+				print('same',oldvalue,newvalue)
+			end 
 		end)
-		Flow.Check(IsPauseMenuActive).OnChangeWhatever(function(datas1,datas2)
-			print("OnChangeWhatever",table.unpack(datas1),table.unpack(datas2))
-		end)
-		Flow.Check(IsPauseMenuActive).OnSame(function(datas)
-			print("OnSame",table.unpack(datas))
-		end)
-		Flow.Check(IsPauseMenuActive).OnSame(function(datas)
-			print("OnNew",table.unpack(datas))
-		end)
-		Flow.Check(IsPauseMenuActive).Update() -- or Flow.Update(IsPauseMenuActive)
-		
 	end 
 end)
--- flowdetector.lua
+++ flowdetector.lua
@@ -51,16 +51,3 @@ Flow.CheckNative = function(name,fn,...)
 	end 
 	
 end 
-
-CreateThread(function()
-	while true do Wait(1000)
-		Flow.CheckNative('haha',GetEntityCoords,PlayerPedId(),function(on)
-			on.change = function(oldvalue,newvalue)
-				print('change',oldvalue,newvalue)
-			end
-			on.same = function(oldvalue,newvalue)
-				print('same',oldvalue,newvalue)
-			end 
-		end)
-	end 
-end)
